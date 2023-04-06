4a2528df53ae535431841615a579d79314d69dd8
huanglinchun <hlcemail@qq.com>
2023-04-06 09:53:28 +0800

    hhh

---
+++ src/views/admin/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+<!--    头部展示及侧边栏展示部分-->
+    
+<!--    主展示部分-->
+    <router-view></router-view>
+<!--    底部展示部分-->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name,
+}
+</script>
+
+<style scoped>
+
+</style>
